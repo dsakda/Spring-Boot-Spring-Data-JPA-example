414f6f1164d81db1403114a4bcf614a8f83e6b73
dsakda <74044765+dsakda@users.noreply.github.com>
2020-12-08 23:02:00 +0700

    Update SpringDataJPAexampleApplication.java

--- src/main/java/com/example/SpringDataJPAexampleApplication.java
+++ src/main/java/com/example/SpringDataJPAexampleApplication.java
@@ -1,13 +1,43 @@
 package com.example;
 
+import com.example.model.Expense;
+import com.example.repository.ExpenseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class SpringDataJPAexampleApplication {
+public class SpringDataJPAexampleApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDataJPAexampleApplication.class, args);
-	}
+    @Autowired
+    ExpenseRepository repository;
 
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJPAexampleApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        repository.save(new Expense("breakfast", 5));
+        repository.save(new Expense("coffee", 2));
+        repository.save(new Expense("New SSD drive", 200));
+        repository.save(new Expense("Tution for baby", 350));
+        repository.save(new Expense("Some apples", 5));
+
+        Iterable<Expense> iterator = repository.findAll();
+
+        System.out.println("All expense items: ");
+        iterator.forEach(item -> System.out.println(item));
+
+        List<Expense> breakfast = repository.findByItem("breakfast");
+        System.out.println("\nHow does my breakfast cost?: ");
+        breakfast.forEach(item -> System.out.println(item));
+
+        List<Expense> expensiveItems = repository.listItemsWithPriceOver(200);
+        System.out.println("\nExpensive Items: ");
+        expensiveItems.forEach(item -> System.out.println(item));
+    }
 }
