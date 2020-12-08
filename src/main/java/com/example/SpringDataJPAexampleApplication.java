812bb4f48335d6f7d87de649322e8590f2639426
dsakda <74044765+dsakda@users.noreply.github.com>
2020-12-08 21:53:45 +0700

    First commit

---
+++ src/main/java/com/example/SpringDataJPAexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJPAexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJPAexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringDataJPAexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJPAexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
